c4b463fcc00eb350580e06928dfd5b0a976b6283
Goldenbwuoy <mumanikidzwag@gmail.com>
2021-04-03 11:39:22 +0800

    setup the hero section

---
+++ client/src/core/HeroSection.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles/HeroSection.css";
+
+function HeroSection() {
+	return (
+		<div className="hero-container">
+			<video src="/videos/video-2.mp4" autoPlay loop mute />
+			<h1>HERE GO AGAIN GUYS</h1>
+			<p>The best of the best is here for you!!!</p>
+		</div>
+	);
+}
+
+export default HeroSection;
--- client/src/core/Home.js
+++ client/src/core/Home.js
@@ -1,60 +1,32 @@
 import React, { useEffect, useState } from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Grid from "@material-ui/core/Grid";
 import { listLatest } from "../auction/api-auction";
-import Suggestions from "../auction/Suggestions";
-import HomeImage from "../assets/images/homeImage.PNG";
 import PopularAuctions from "../auction/PopularAuctions";
-import { Card, CardMedia } from "@material-ui/core";
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-    margin: 30,
-  },
-  imageCard: {
-    maxWidth: "100%",
-  },
-  media: {
-    minHeight: 450,
-  },
-}));
+import HeroSection from "./HeroSection";
 
 function Home() {
-  const classes = useStyles();
-  const [latestAuctions, setLatestAuctions] = useState([]);
+	const [latestAuctions, setLatestAuctions] = useState([]);
 
-  useEffect(() => {
-    const abortController = new AbortController();
-    const signal = abortController.signal;
+	useEffect(() => {
+		const abortController = new AbortController();
+		const signal = abortController.signal;
 
-    listLatest(signal).then((data) => {
-      if (data && data.error) {
-        console.log(data.error);
-      } else {
-        setLatestAuctions(data);
-      }
-    });
+		listLatest(signal).then((data) => {
+			if (data && data.error) {
+				console.log(data.error);
+			} else {
+				setLatestAuctions(data);
+			}
+		});
 
-    return function cleanup() {
-      abortController.abort();
-    };
-  }, []);
-  return (
-    <div className={classes.root}>
-      <Grid container spacing={2}>
-        <Grid item xs={8} sm={8}>
-          <Card className={classes.imageCard}>
-            <CardMedia className={classes.media} image={HomeImage} />
-          </Card>
-          <PopularAuctions />
-        </Grid>
-        <Grid item xs={4} sm={4}>
-          <Suggestions auctions={latestAuctions} title="Latest Auctions" />
-        </Grid>
-      </Grid>
-    </div>
-  );
+		return function cleanup() {
+			abortController.abort();
+		};
+	}, []);
+	return (
+		<>
+			<HeroSection />
+		</>
+	);
 }
 
 export default Home;
