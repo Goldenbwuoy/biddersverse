46f00c0ffc384c3d35cb35e8212e6a596bea89e3
Goldenbwuoy <mumanikidzwag@gmail.com>
2021-04-03 17:34:41 +0800

    setup hero section search bar

--- client/src/core/HeroSection.js
+++ client/src/core/HeroSection.js
@@ -5,8 +5,10 @@ function HeroSection() {
 	return (
 		<div className="hero-container">
 			<video src="/videos/video-2.mp4" autoPlay loop />
-			<h1>HERE GO AGAIN GUYS</h1>
-			<p>The best of the best is here for you!!!</p>
+			<div className="hero-section-div">
+				<p>Leader in Electronics Auctions</p>
+				<input type="text" placeholder="Search Auctions..." />
+			</div>
 		</div>
 	);
 }
